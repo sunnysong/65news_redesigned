bf0b555520d5b1a1820af21a110945bae1b4ee10
sunnysong <sunnysong1987@gmail.com>
2015-07-20 21:23:49 +0800

    nothing

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 import os
 import psycopg2
-import urllib.parse as urlparse
+from urllib.parse import urlparse
 
 urlparse.uses_netloc.append("postgres")
 # url = urlparse.urlparse(os.environ["DATABASE_URL"])
@@ -12,7 +12,7 @@ class Config(object):
     CSRF_ENABLED = True
     SECRET_KEY = 'findhowtomakeasecretekey'
     POSTS_PER_PAGE = 10
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL') or urlparse.urlparse(os.environ["DATABASE_URL"]) 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URL') or urlparse(os.environ["DATABASE_URL"]) 
 
     @staticmethod
     def init_app(app):
