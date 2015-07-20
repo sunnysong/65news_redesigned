0f8a47db9b9e1b567b3d80010ef68fe30eb5aeef
sunnysong <sunnysong1987@gmail.com>
2015-07-20 21:03:22 +0800

    nothing

--- manage.py
+++ manage.py
@@ -41,6 +41,16 @@ def make_shell_context():
 manager.add_command('shell', Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
+    # create user roles
+    Role.insert_roles()
+    # create self-follows for all users
+
+
 
 if __name__ == '__main__':
 	manager.run()
